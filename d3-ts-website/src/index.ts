eb64c97d99c84489a71d387e598d4cccefcf5507
whats2000 <whats2000mc@gmail.com>
2024-05-21 15:17:12 +0800

    Add: Set up data load pipeline

--- d3-ts-website/src/index.ts
+++ d3-ts-website/src/index.ts
@@ -1,43 +1,25 @@
 import * as d3 from 'd3';
 
-const data = [10, 20, 30, 40, 50];
+const loadChunk = async (chunkNumber: number) => {
+  const response = await fetch(`./data/chunk_${chunkNumber}.json`);
+  if (!response.ok) {
+    throw new Error(`Failed to load chunk ${chunkNumber}`);
+  }
+  return response.json();
+};
 
-const width = 500;
-const height = 500;
-const margin = { top: 20, right: 30, bottom: 40, left: 40 };
+const loadAllData = async () => {
+  const chunkCount = 9;
+  const dataPromises = [];
+  for (let i = 0; i < chunkCount; i++) {
+    dataPromises.push(loadChunk(i));
+  }
+  const dataArrays = await Promise.all(dataPromises);
+  return dataArrays.flat();
+};
 
-const svg = d3.select('body')
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height)
-  .append('g')
-  .attr('transform', `translate(${margin.left},${margin.top})`);
-
-const x = d3.scaleBand()
-  .domain(data.map((_, i) => i.toString()))
-  .range([0, width - margin.left - margin.right])
-  .padding(0.1);
-
-const y = d3.scaleLinear()
-  .domain([0, d3.max(data) as number])
-  .nice()
-  .range([height - margin.top - margin.bottom, 0]);
-
-svg.append('g')
-  .attr('class', 'x-axis')
-  .attr('transform', `translate(0,${height - margin.top - margin.bottom})`)
-  .call(d3.axisBottom(x));
-
-svg.append('g')
-  .attr('class', 'y-axis')
-  .call(d3.axisLeft(y));
-
-svg.selectAll('.bar')
-  .data(data)
-  .enter()
-  .append('rect')
-  .attr('class', 'bar')
-  .attr('x', (_, i) => x(i.toString()) as number)
-  .attr('y', d => y(d))
-  .attr('width', x.bandwidth())
-  .attr('height', d => height - margin.top - margin.bottom - y(d));
+loadAllData().then(data => {
+  console.log(data);
+}).catch(error => {
+  console.error('Error loading data:', error);
+});
--- d3-ts-website/webpack.config.js
+++ d3-ts-website/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, argv) => ({
     entry: './src/index.ts',
@@ -10,6 +11,28 @@ module.exports = (env, argv) => ({
                 test: /\.ts$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
+            },
+            {
+                test: /\.csv$/,
+                use: [
+                    {
+                        loader: 'csv-loader',
+                        options: {
+                            dynamicTyping: true,
+                            header: true,
+                            skipEmptyLines: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.json$/,
+                type: 'javascript/auto',
+                use: [
+                    {
+                        loader: 'json-loader',
+                    },
+                ],
             },
         ],
     },
@@ -29,6 +52,11 @@ module.exports = (env, argv) => ({
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/data', to: 'data' },
+            ],
         }),
     ],
 });
